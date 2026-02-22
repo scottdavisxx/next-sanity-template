5517e4115bf369df1a1729e943874a96e81a1767
Scott Davis <scottdavisxx@gmail.com>
2026-02-21 17:09:33 -0700

    add css animations to threeColCards

--- frontend/app/components/ThreeColCards.tsx
+++ frontend/app/components/ThreeColCards.tsx
@@ -9,21 +9,18 @@ export default function ThreeColCards() {
       image: "/three-col-card-1.png",
       altText: "Academic Excellence",
       href: "/academic-excellence",
-      linkText: "Learn More"
     },
     {
       title: "Purpose & Belonging",
       image: "/three-col-card-2.png",
       altText: "Purpose & Belonging",
       href: "/academic-excellence",
-      linkText: "Learn More"
     },
     {
       title: "Faith Formation",
       image: "/three-col-card-3.png",
       altText: "Academic Excellence",
       href: "/academic-excellence",
-      linkText: "Learn More"
     }
   ]
 
@@ -31,7 +28,7 @@ export default function ThreeColCards() {
   return (
     <div className="flex px-6 py-8 gap-2 justify-between h-[478px]">
       {cards.map((card) => (
-        <div key={card.title} className="flex flex-col gap-2 border border-gray rounded-xl relative w-1/3 items-center justify-center text-black text-4xl font-bold">
+        <Link href={card.href} key={card.title} className="group flex flex-col gap-2 border border-gray rounded-xl relative w-1/3 items-center justify-center text-black text-4xl font-bold hover:-translate-y-2 transition-all duration-300">
           <Image
             src={card.image}
             alt={card.altText}
@@ -39,9 +36,11 @@ export default function ThreeColCards() {
             height={478}
             className="absolute top-0 left-0 h-full w-full object-cover rounded-xl"
           />
+          {/* Image overlay */}
+          <div className="absolute top-0 left-0 h-full w-full bg-white/45 rounded-xl group-hover:bg-white/75 transition-all duration-300"></div>
           <h3 className="z-2">{card.title}</h3>
-          <Link href={card.href} className="z-2">{card.linkText}</Link>
-        </div>
+          <span className="z-2 group-hover:translate-x-2 transition-all duration-300">→ Learn More</span>
+        </Link>
       ))}
     </div>
   )
