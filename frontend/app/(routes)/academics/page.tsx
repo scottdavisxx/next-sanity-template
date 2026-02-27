8a708a4382f0397de2c9e568f7a5aff9e56272ab
Jose Barajas <barajas.jose45@gmail.com>
2026-02-26 17:50:08 -0800

    components for the website

--- frontend/app/(routes)/academics/page.tsx
+++ frontend/app/(routes)/academics/page.tsx
@@ -1,6 +1,8 @@
 import HeroBanner from "@/app/components/HeroBanner";
 import Navigation from "@/app/components/Navigation";
 import Subnav from "@/app/components/Subnav";
+import Intro from "@/app/components/IntroBlade";
+import ImageWithText from "@/app/components/ImageWithText";
 
 const tempNavigationContent = {
   "_key": "9cb3816cfaf4",
@@ -30,6 +32,44 @@ const tempHeroContent = {
   "titleTwo": "Test Title Two"
 }
 
+const intro = {
+    titles: [
+        {
+            title: "EARLY EDUCATION",
+        },
+        {
+            title: "ELEMENTARY SCHOOL",
+
+        },
+        {
+            title: "MIDDLE SCHOOL",
+        }
+    ],
+    blurb: 'Our Redeemer Lutheran School of Dallas serves Preschool through 8th grade with a high-quality, individualized education that develops strong academics, strong character, and confident learners prepared for what’s next.',
+    ctas: [
+        {
+            buttonText: 'Schedule A tour',
+            link: '/'
+        },
+        {
+            buttonText: 'Inquire',
+            link: '/'
+        }
+    ],
+    bgImage: "/bg-pattern.png",
+
+}
+
+const imageWithText = {
+    imageSrc: "/children-studying.png",
+    imageAlt: "Children studying together",
+    title: "The OR Dallas Difference",
+    blurb: "OR Dallas is a Christ-centered school where students are known personally and challenged thoughtfully. We combine strong instruction, purposeful formation, and a culture of belonging to help students grow with clarity, confidence, and integrity."
+}
+
+
+
+
 export default function AcademicsPage() {
   return (
     <>
@@ -38,6 +78,8 @@ export default function AcademicsPage() {
       {/* @ts-ignore */}
       <HeroBanner block={tempHeroContent} />
       <Subnav />
+      <Intro {...intro} />
+      <ImageWithText {...imageWithText}/>
     </>
   )
 }
---
+++ frontend/app/components/IntroBlade.tsx
@@ -0,0 +1,49 @@
+import Image from "next/image"
+import Link from "next/link"
+import Cta from "./ui/Cta"
+
+
+export interface IntroProps {
+  titles: Array<{ title: string }>;
+  blurb: string;
+  ctas?: Array<{ buttonText: string; link: string }>;
+  bgImage: string;
+}
+
+export default function Intro({ titles, blurb, ctas, bgImage }: IntroProps) {
+
+
+
+
+    return (
+        <div className="relative flex flex-col px-24 py-8 gap-2  justify-between min-h-[478px]">
+            <div className="absolute inset-0 left-0 right-0 w-full h-full overflow-hidden">
+                <Image
+                    src={bgImage}
+                    alt={'pattern'}
+                    fill
+                    className="object-cover opacity-75" />
+
+            </div>
+            <div className="relative flex flex-col md:flex-row px-6 py-8 gap-8 md:justify-between items-center z-10">
+                {
+                    titles.map((obj) => {
+                        return <h2 key={obj.title} className="text-4xl font-bold w-full md:w-1/3 text-center">{obj.title}</h2>
+                    })
+                }
+            </div>
+            <div className="relative flex px-6 py-8 gap-2 justify-between items-center z-10">
+                <p className="text-[2rem] text-center">{blurb}</p>
+            </div>
+            <div className="relative flex flex-col md:flex-row px-6 py-8 gap-8 justify-center items-center z-10">
+                {
+                    ctas?.map((cta) => {
+                        return <Cta key={cta.buttonText} href={cta.link} buttonText={cta.buttonText} dark={true} buttonColor="brand-black" />
+                    })
+                }
+            </div>
+
+
+        </div>
+    )
+}
