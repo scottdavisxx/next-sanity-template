80aeae01a8eba3f3df0e4b6d2f2d0ce1933cd978
Scott Davis <scottdavisxx@gmail.com>
2026-02-28 15:14:13 -0700

    add embedded calendar to athletics page

---
+++ frontend/app/components/Calendar.tsx
@@ -0,0 +1,10 @@
+export default function Calendar() {
+  return (
+    <div className="flex justify-center">
+      <iframe src="https://calendar.google.com/calendar/embed?src=ordallas.org_01guksd5i63kb6o1sigjtgd120%40group.calendar.google.com&ctz=America%2FChicago"
+        style={{ border: 0 }} width="1400" height="750"
+        scrolling="no">
+      </iframe>
+    </div>
+  )
+}
