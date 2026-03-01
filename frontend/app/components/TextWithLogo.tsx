e8e6200260246615b1396860f4a7fb77301c99e0
Scott Davis <scottdavisxx@gmail.com>
2026-02-28 19:07:22 -0700

    Enhance layout with full-width container and adjust subnav z-index for better visibility

--- frontend/app/components/TextWithLogo.tsx
+++ frontend/app/components/TextWithLogo.tsx
@@ -9,11 +9,13 @@ interface TextWithLogoProps {
 export default function TextWithLogo({ title, blurb }: TextWithLogoProps) {
 
   return (
-    <div className="bg-white flex flex-col gap-4 text-black py-16 pl-24 relative overflow-hidden">
-      <h2 className="text-6xl font-bold w-3/5 leading-20">{title}</h2>
-      <p className="w-3/5">{blurb}</p>
-      <div className="absolute -right-48 -top-1/5">
-        <OrlsIcon width={513} height={513} color="light-blue" />
+    <div className="container">
+      <div className="bg-white flex flex-col gap-4 text-black py-16 relative">
+        <h2 className="text-6xl font-bold w-3/5 leading-20">{title}</h2>
+        <p className="w-3/5">{blurb}</p>
+        <div className="absolute -right-48 -top-1/5">
+          <OrlsIcon width={513} height={513} color="light-blue" />
+        </div>
       </div>
     </div>
   )
