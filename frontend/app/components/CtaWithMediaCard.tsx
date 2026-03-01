9b20ed814f0c2ad36396f7ce5e3b9677599a3af7
Scott Davis <scottdavisxx@gmail.com>
2026-02-28 21:44:51 -0700

    Refactor CtaWithCard, CtaWithMediaCard, FeaturedEvents, and FourColStatistics components for improved layout and mobile responsiveness; adjust structure and styling for better visual consistency

--- frontend/app/components/CtaWithMediaCard.tsx
+++ frontend/app/components/CtaWithMediaCard.tsx
@@ -18,33 +18,35 @@ export interface CtaWithMediaCardProps {
 
 export default function CtaWithMediaCard({ title, blurb, image, altText }: CtaWithMediaCardProps) {
   return (
-    <div className="flex flex-col md:flex-row items-center justify-center py-8 px-2 relative
-    md:px-24 md:py-16">
-      {/* Content */}
-      <div className="py-2 border-2 border-dark-blue rounded-t-4xl md:rounded-l-4xl md:rounded-tr-none w-full md:w-[40%] border-b-0 
+    <div className="bg-white py-4
+    md:py-16">
+      <div className="flex flex-col md:flex-row items-center justify-center py-8 relative container">
+        {/* Content */}
+        <div className="py-2 border-2 border-dark-blue rounded-t-4xl md:rounded-l-4xl md:rounded-tr-none w-full md:w-[40%] border-b-0 
       md:border-b-2 md:border-r-0 md:py-8">
-        <div className="flex flex-col pl-4 pr-4 py-4 md:pl-8 md:pr-0 items-start z-10
+          <div className="flex flex-col pl-4 pr-4 py-4 md:pl-8 md:pr-0 items-start z-10
         md:py-8">
-          <h2 className="text-2xl font-bold text-medium-blue leading-tight w-full md:w-3/4">{title}</h2>
-          <p className="text-lg whitespace-pre-line w-full md:w-3/4">{blurb}</p>
-          {block.cta && (
-            <div className="flex items-center gap-1 mt-4">
-              <CtaWithIcon
-                href={block.cta.href || "#"}
-                buttonText={block.cta.buttonText || "Learn More"}
-                newTab={block.cta.newTab}
-              />
-            </div>
-          )}
+            <h2 className="text-2xl font-bold text-medium-blue leading-tight w-full md:w-3/4">{title}</h2>
+            <p className="text-lg whitespace-pre-line w-full md:w-3/4">{blurb}</p>
+            {block.cta && (
+              <div className="flex items-center gap-1 mt-4">
+                <CtaWithIcon
+                  href={block.cta.href || "#"}
+                  buttonText={block.cta.buttonText || "Learn More"}
+                  newTab={block.cta.newTab}
+                />
+              </div>
+            )}
+          </div>
         </div>
-      </div>
-      <div className="border-2 border-dark-blue rounded-b-4xl bg-white z-10 w-full overflow-hidden border-t-0
+        <div className="border-2 border-dark-blue rounded-b-4xl bg-white z-10 w-full overflow-hidden border-t-0
       md:border-t-2 md:rounded-4xl md:w-[60%]">
-        <Image
-          src={image}
-          alt={altText}
-          width={698} height={533}
-          className="relative z-10 rounded-b-4xl md:rounded-4xl w-full object-cover" />
+          <Image
+            src={image}
+            alt={altText}
+            width={698} height={533}
+            className="relative z-10 rounded-b-4xl md:rounded-4xl w-full object-cover" />
+        </div>
       </div>
     </div>
   )
--- frontend/app/components/FourColStatistics.tsx
+++ frontend/app/components/FourColStatistics.tsx
@@ -1,39 +1,41 @@
 export default function FourColStatistics() {
   return (
-    <div className="text-white bg-white">
-      <div className="flex flex-col justify-between bg-dark-blue px-6 py-8 items-center
+    <div className="bg-dark-blue">
+      <div className="text-white container">
+        <div className="flex flex-col justify-between bg-dark-blue px-6 py-8 items-center
       md:flex-row md:px-16 md:py-20">
-        <div className="flex flex-col justify-between text-center
+          <div className="flex flex-col justify-between text-center
         md:w-8/12 md:flex-row">
-          <div className="flex flex-col gap-1 py-4 px-3 border-white
+            <div className="flex flex-col gap-1 py-4 px-3 border-white
           md:border-r">
-            <span className="text-4xl font-bold
+              <span className="text-4xl font-bold
             md:text-5xl">1,850</span>
-            <span className="text-2xl">Schools Nationwide</span>
-          </div>
-          <div className="flex flex-col gap-1 py-4 px-3 border-white
+              <span className="text-2xl">Schools Nationwide</span>
+            </div>
+            <div className="flex flex-col gap-1 py-4 px-3 border-white
           md:border-r">
-            <span className="text-4xl font-bold
+              <span className="text-4xl font-bold
             md:text-5xl">90</span>
-            <span className="text-2xl">Countries on 5 Continents</span>
-          </div>
-          <div className="flex flex-col gap-1 py-4 px-3 border-white
+              <span className="text-2xl">Countries on 5 Continents</span>
+            </div>
+            <div className="flex flex-col gap-1 py-4 px-3 border-white
           md:border-r">
-            <span className="text-4xl font-bold
+              <span className="text-4xl font-bold
             md:text-5xl">162,000</span>
-            <span className="text-2xl">Students Nationwide</span>
-          </div>
-          <div className="flex flex-col gap-1 py-4 px-3">
-            <span className="text-4xl font-bold
+              <span className="text-2xl">Students Nationwide</span>
+            </div>
+            <div className="flex flex-col gap-1 py-4 px-3">
+              <span className="text-4xl font-bold
             md:text-5xl">39%</span>
-            <span className="text-2xl">Lutheran Students</span>
+              <span className="text-2xl">Lutheran Students</span>
+            </div>
           </div>
-        </div>
-        <div className="flex flex-col gap-4 order-first
+          <div className="flex flex-col gap-4 order-first
         md:order-last md:w-1/4">
-          <h3 className="text-4xl font-bold
+            <h3 className="text-4xl font-bold
           md:text-5xl">Did you know?</h3>
-          <p className="text-2xl"> LCMS schools include 1,600+ early childhood centers and 800+ elementary schools nationwide.</p>
+            <p className="text-2xl"> LCMS schools include 1,600+ early childhood centers and 800+ elementary schools nationwide.</p>
+          </div>
         </div>
       </div>
     </div>
