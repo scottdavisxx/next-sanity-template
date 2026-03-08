b01e77d90d1ce02407e09e660d911b3ceaba075e
Scott Davis <scottdavisxx@gmail.com>
2026-03-08 11:20:06 -0600

    TwoCtasWithImage first changes for mobile

--- frontend/app/components/TwoCtasWithImage.tsx
+++ frontend/app/components/TwoCtasWithImage.tsx
@@ -20,12 +20,13 @@ export default function TwoCtasWithImage() {
           alt="Two Ctas With Image"
           width={439}
           height={633}
-          className="absolute bottom-0 left-16"
+          className="absolute bottom-0 hidden md:block md:left-16"
         />
         {/* Content */}
-        <div className="flex flex-col justify-between gap-6 z-10
+        <div className="flex flex-col justify-between gap-6 z-10 py-4
         md:w-[60%] md:px-10 md:py-12">
-          <h2 className="text-7xl font-bold leading-tight">Start Your Admissions Journey</h2>
+          <h2 className="text-4xl  font-bold leading-tight text-balance
+          md:text-7xl">Start Your Admissions Journey</h2>
           <p className="text-xl">Choosing a school is a big decision and it is personal. At Our Redeemer, strong academics, strong character, and deep faith in a joyful community are partnered with families to form students who think well, live faithfully, and lead with integrity.</p>
           {/* CTAs */}
           <div className="flex flex-col gap-8 text-center text-balance 
@@ -36,7 +37,9 @@ export default function TwoCtasWithImage() {
                 href="#"
                 buttonText="Request Information"
                 buttonColor="brand-blue"
-                font="small" />
+                font="small"
+                className="w-full md:w-auto"
+              />
             </div>
             <div className="flex flex-col gap-2 items-center">
               <p>Attend a visit day, come to Open House, or take a campus tour.</p>
@@ -44,7 +47,9 @@ export default function TwoCtasWithImage() {
                 href="#"
                 buttonText="Schedule Visit"
                 buttonColor="brand-blue"
-                font="small" />
+                font="small"
+                className="w-full md:w-auto"
+              />
             </div>
           </div>
         </div>
--- frontend/app/static/scott/page.tsx
+++ frontend/app/static/scott/page.tsx
@@ -3,6 +3,7 @@ import TwoCtasWithImage from "@/app/components/TwoCtasWithImage";
 export default function ScottPage() {
   return (
     <>
+      <div className="w-full h-[100px] bg-white"></div>
       <TwoCtasWithImage />
     </>
   )
