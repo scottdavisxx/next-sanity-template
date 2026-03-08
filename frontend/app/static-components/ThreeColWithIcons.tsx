d41c4886e6e1ec8f065a82e620a32ffe08fc39d7
Scott Davis <scottdavisxx@gmail.com>
2026-03-08 16:41:02 -0600

    make border 2px wide

--- frontend/app/static-components/ThreeColWithIcons.tsx
+++ frontend/app/static-components/ThreeColWithIcons.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function ThreeColWithIcons() {
   return (
-    <div className="bg-dark-blue py-4 lg:py-16 text-white">
+    <div className="bg-dark-blue py-4 lg:py-12 text-white">
       <div className="container flex flex-col gap-4 items-center justify-between">
         <h2 className="text-4xl font-bold lg:text-7xl">Individualized Support</h2>
         <p className="text-xl text-center 
@@ -14,7 +14,7 @@ export default function ThreeColWithIcons() {
         lg:flex-row">
           {/* Column 1 */}
           <div className="flex flex-col justify-between gap-8 py-6
-          lg:border-r lg:border-white lg:max-w-1/3 lg:px-4">
+          lg:border-r-2 lg:border-white lg:max-w-1/3 lg:px-4">
             <div className="flex items-center gap-4">
               <Image src="/threeColTemp.png" alt="Icon 1" width={100} height={100} />
               <p className="text-3xl font-bold w-fit">On-Site Tutoring</p>
@@ -26,7 +26,7 @@ export default function ThreeColWithIcons() {
           </div>
           {/* Column 2 */}
           <div className="flex flex-col justify-between gap-8 py-6
-          lg:border-r lg:border-white lg:max-w-1/3 lg:px-4">
+          lg:border-r-2 lg:border-white lg:max-w-1/3 lg:px-4">
             <div className="flex items-center gap-4">
               <Image src="/threeColTemp.png" alt="Icon 1" width={100} height={100} />
               <p className="text-3xl font-bold w-fit">Personalized Learning Plans</p>
