1c7d1b899acad59954c50d54535374e38a2f8707
Scott Davis <scottdavisxx@gmail.com>
2026-03-10 20:38:21 -0600

    fix issue on mobile with TextWithLogo

--- frontend/app/components/TextWithLogo.tsx
+++ frontend/app/components/TextWithLogo.tsx
@@ -14,15 +14,17 @@ export default function TextWithLogo({ block }: TextWithLogoProps) {
   const blurb = block?.blurb
 
   return (
-    <div className="container">
-      <div className="flex flex-col gap-4 text-black py-6 relative
+    <div className="overflow-hidden">
+      <div className="container">
+        <div className="flex flex-col gap-4 text-black py-6 relative
       md:overflow-visible md:py-16 md:px-0">
-        <h2 className="text-4xl font-bold md:text-6xl md:leading-20 md:w-3/5">{title}</h2>
-        <div className="md:w-3/5 prose prose-lg max-w-none">
-          {Array.isArray(blurb) ? <PortableText value={blurb as PortableTextBlock[]} /> : blurb && <p>{blurb}</p>}
-        </div>
-        <div className="absolute -right-48 -top-1/5 -z-10">
-          <OrlsIcon width={513} height={513} color="light-blue" />
+          <h2 className="text-4xl font-bold md:text-6xl md:leading-20 md:w-3/5">{title}</h2>
+          <div className="md:w-3/5 prose prose-lg max-w-none">
+            {Array.isArray(blurb) ? <PortableText value={blurb as PortableTextBlock[]} /> : blurb && <p>{blurb}</p>}
+          </div>
+          <div className="absolute -right-48 -top-1/5 -z-10">
+            <OrlsIcon width={513} height={513} color="light-blue" />
+          </div>
         </div>
       </div>
     </div>
